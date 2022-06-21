de70e59ad1bcaa45cb4647537a93a1337ea7690d
gelald <yb.ng@foxmail.com>
2022-06-21 22:05:36 +0800

    Update RocketMQOrderConsumer.java

--- rocketmq-boot-demo/rocketmq-boot-demo-consumer/src/main/java/com/example/rocketmq/boot/RocketMQOrderConsumer.java
+++ rocketmq-boot-demo/rocketmq-boot-demo-consumer/src/main/java/com/example/rocketmq/boot/RocketMQOrderConsumer.java
@@ -5,7 +5,6 @@ import org.apache.rocketmq.common.message.MessageExt;
 import org.apache.rocketmq.spring.annotation.ConsumeMode;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
-import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
